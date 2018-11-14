0d08fec3b8740bc25fc2227220109d7b9f789701
song <wichasha13@gmail.com>
2018-11-14 17:11:54 +0000

    edit space case

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,7 @@ class BlogsController < ApplicationController
   def index
     @blog = Blog.all
   end
+  
   def new
     if params[:back]
       @blog = Blog.new(blog_params)
@@ -13,6 +14,7 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
+  
   def create
     @blog = Blog.new(blog_params)
     @blog.user_id = current_user.id
@@ -22,15 +24,16 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
+  
   def show
     @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
-  def edit
-  end
+  
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
+  
   def update
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
@@ -38,23 +41,31 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
+  
   def confirm
     @blog = Blog.new(blog_params)
+    if @blog.title =="" || @blog.content ==""
+      flash[:error] =  "内容を入力してください"
+      redirect_to new_blog_path
+    end
   end
   
   private
   def blog_params
     params.require(:blog).permit(:title, :content, :@curuser)
   end
+  
   def set_blog
     @blog = Blog.find(params[:id])
   end
+  
   def login_check
     unless logged_in?
       flash[:error] =  "ログインしてください"
       redirect_to new_session_path
     end
   end
+  
   def current_check
     if @blog.user_id != current_user.id
       flash[:error] =  "current error"
