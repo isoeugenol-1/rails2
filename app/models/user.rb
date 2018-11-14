47a88929c0d57ace771790ed4336fc60d438b9ed
song <wichasha13@gmail.com>
2018-11-14 12:03:11 +0000

    add belongto to blog.rb,user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_many :favorites, dependent: :destroy
   has_many :favorite_blogs, through: :favorites, source: :blog
+  belongs_to :blog
 end
