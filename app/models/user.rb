20f7beb8c58839c0d64300902ad351aa7cd5a208
song <wichasha13@gmail.com>
2018-11-17 09:35:07 +0000

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_many :favorites, dependent: :destroy
   has_many :favorite_blogs, through: :favorites, source: :blog
-  belongs_to :blog
 end
