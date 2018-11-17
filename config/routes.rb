7ba13ce46806cf815568582e1b3bbe1f5b831b42
song <wichasha13@gmail.com>
2018-11-17 11:02:01 +0000

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :favorites, only: [:create, :destroy]
