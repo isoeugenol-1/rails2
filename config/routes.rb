714a0bf9f78bbbbab06a103a73cf360181427e70
song <wichasha13@gmail.com>
2018-11-17 10:25:21 +0000

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,6 @@ Rails.application.routes.draw do
   get    'login'    ,to: 'users#new'
   get    'top'      ,to: 'blogs#top'
   get    '/blogs/confirm'  ,to: 'blogs#confirm'
-  get    '/blogs'          ,to: 'blogs#index'
-  post   '/blogs'          ,to: 'blogs#create'
-  get    '/blogs/new'      ,to: 'blogs#new'
-  get    '/blogs/:id/edit' ,to: 'blogs#edit'
-  get    '/blogs/:id'      ,to: 'blogs#show'
-  patch  '/blogs/:id'      ,to: 'blogs#update'
-  delete '/blogs/:id'      ,to: 'blogs#destroy'
   get 'sessions/new'
   get 'favorites'         ,to: 'favorites#show'
   resources :contacts
