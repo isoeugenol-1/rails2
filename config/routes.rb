7056eb577a24c1b85263e2690f2ed47820de9fc1
song <wichasha13@gmail.com>
2018-11-19 10:05:14 +0000

    no letter

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :favorites
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  #mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
